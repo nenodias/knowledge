47b5befc13b4f7ecbe8f8b1ec59e05f9e0dca3e9
Horácio Dias <horacio.dias92@gmail.com>
2018-12-18 13:22:17 -0200

    Atualizando o projeto com autenticação JWT usando o passport

--- versao-inicial/backend/config/routes.js
+++ versao-inicial/backend/config/routes.js
@@ -3,36 +3,46 @@ module.exports = app => {
     app.post('/signin', app.api.auth.signin);
     app.post('/validateToken', app.api.auth.validateToken);
     
+    const authenticate = app.config.passport.authenticate();
+
     app.route('/users')
+        .all(authenticate)
         .post(app.api.user.save)
         .get(app.api.user.get);
     app.route('/users/:id')
+        .all(authenticate)
         .put(app.api.user.save)
         .get(app.api.user.getById);
     
 
     app.route('/categories')
+        .all(authenticate)
         .post(app.api.category.save)
         .get(app.api.category.get);
     
     app.route('/categories/tree')
+        .all(authenticate)
         .get(app.api.category.getTree);
 
     app.route('/categories/:id')
+        .all(authenticate)  
         .put(app.api.category.save)
         .delete(app.api.category.remove)
         .get(app.api.category.getById);
     
     
     app.route('/articles')
+        .all(authenticate)
         .post(app.api.article.save)
         .get(app.api.article.get);
     
     app.route('/articles/:id')
+        .all(authenticate)
         .put(app.api.article.save)
         .delete(app.api.article.remove)
         .get(app.api.article.getById);
 
     app.route('/categories/:id/articles')
+        .all(authenticate)
         .get(app.api.article.getByCategory);
 }
