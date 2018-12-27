066f52585eeec055f0e987065a9cc94bb6551b78
Horácio Dias <horacio.dias92@gmail.com>
2018-12-27 18:00:11 +0000

    Finalizando o pagetitle

---
+++ versao-inicial/frontend/src/components/templates/PageTitle.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="page-title">
+        <h1>
+            <i :class="icon" v-if="icon"></i> {{ main }}
+        </h1>
+        <h2>{{ sub }}</h2>
+        <hr />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PageTitle',
+    props: {
+        icon: String,
+        main: String,
+        sub: String
+    }
+}
+</script>
+
+<style>
+.page-title h1 {
+    margin: 0;
+}
+
+.page-title h2 {
+    color: #777;
+    margin-top: 5px;
+    font-size: 1.3 rem;
+}
+</style>
