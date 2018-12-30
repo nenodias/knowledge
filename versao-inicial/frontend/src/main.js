196e1d453fe9c3822a373ad57aeb177b73d35626
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2018-12-30 12:21:54 -0200

    Iniciando o cadastro do usuário

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -9,7 +9,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 //Temporário
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MywibmFtZSI6IkhvcsOhY2lvIERpYXMgQmFwdGlzdGEgTmV0byIsImVtYWlsIjoiaG9yYWNpby5kaWFzOTJAZ21haWwuY29tIiwiYWRtaW4iOmZhbHNlLCJpYXQiOjE1NDYwMjgzNzcsImV4cCI6MTU0NjI4NzU3N30.oqf5Rmex0YI9t-n1FxkEYbHOBdCRxAaEH25M4mbpW7w';
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MywibmFtZSI6IkhvcsOhY2lvIERpYXMgQmFwdGlzdGEgTmV0byIsImVtYWlsIjoiaG9yYWNpby5kaWFzOTJAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU0NjE3OTM1MSwiZXhwIjoxNTQ2NDM4NTUxfQ.WsO2GNFgoHY9To3aOWbpKtgkswRaFOXJpOUq-DjLhj4';
 
 new Vue({
   store,
