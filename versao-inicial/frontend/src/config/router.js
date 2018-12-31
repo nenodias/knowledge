32688a35c1b58acff19eed60062fb23caaf39098
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2018-12-31 14:37:55 -0200

    Exibindo artigo por id

--- versao-inicial/frontend/src/config/router.js
+++ versao-inicial/frontend/src/config/router.js
@@ -4,13 +4,15 @@ import Vue from 'vue';
 import Home from '@/components/home/Home';
 import AdminPages from '@/components/admin/AdminPages';
 import ArticlesByCategory from '@/components/article/ArticlesByCategory';
+import ArticleById from '@/components/article/ArticleById';
 
 Vue.use(VueRouter);
 
 const routes = [
     { name:'home', path: '/', component: Home },
     { name:'adminPages', path: '/admin', component: AdminPages },
-    { name:'articleByCategory', path: '/categories/:id/articles', component: ArticlesByCategory }
+    { name:'articlesByCategory', path: '/categories/:id/articles', component: ArticlesByCategory },
+    { name:'articleById', path: '/articles/:id', component: ArticleById }
 ];
 
 const router = new VueRouter({
